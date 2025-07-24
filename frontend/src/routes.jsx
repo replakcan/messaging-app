726f44d30ebf647e35a91f1a0e98aa3916522059
replakcan <alper.makcan@gmail.com>
2025-07-24 19:03:01 +0300

    Removes children routes

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,12 +1,11 @@
-import ErrorPage from "./routes/error-page"
-import Root from "./routes/root"
+import ErrorPage from './routes/error-page'
+import Root from './routes/root'
 
 const routes = [
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
-    children: [],
   },
 ]
 
--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,10 +1,3 @@
-import { Outlet } from 'react-router'
-
 export default function Root() {
-  return (
-    <>
-      <h1>root</h1>
-      <Outlet />
-    </>
-  )
+  return <section className="main-container"></section>
 }

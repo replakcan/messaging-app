28a10cf7dfe230bdb5135c6e3e3c3050cc09b19e
replakcan <alper.makcan@gmail.com>
2025-07-25 17:47:29 +0300

    Updates backend routes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -49,6 +49,21 @@ exports.addNewFriend = async (req, res, next) => {
   }
 }
 
+exports.getFriendById = async (req, res, next) => {
+  const { friendId } = req.params
+
+  try {
+    const friend = await prisma.user.findUniqueOrThrow({
+      where: { id: friendId },
+      omit: { password: true },
+    })
+
+    res.json(friend)
+  } catch (error) {
+    next(error)
+  }
+}
+
 exports.removeExistingFriend = async (req, res, next) => {
   const { user } = req
   const { friendId } = req.params
@@ -102,8 +117,28 @@ exports.getUserGroups = async (req, res, next) => {
     const groups = await prisma.user.findFirstOrThrow({
       where: { id: user.id },
       select: {
-        adminOfGroups: true,
-        memberOfGroups: true,
+        adminOfGroups: {
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            members: true,
+            createdAt: true,
+            adminId: true,
+            admin: { select: { id: true, phone: true } },
+          },
+        },
+        memberOfGroups: {
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            members: true,
+            createdAt: true,
+            adminId: true,
+            admin: { select: { id: true, phone: true } },
+          },
+        },
       },
     })
 
@@ -138,7 +173,15 @@ exports.getGroupById = async (req, res, next) => {
   try {
     const group = await prisma.group.findFirstOrThrow({
       where: { id: groupId },
-      include: { messages: true },
+      include: {
+        messages: true,
+        members: true,
+        admin: {
+          select: {
+            phone: true,
+          },
+        },
+      },
     })
 
     res.json(group)
@@ -208,7 +251,7 @@ exports.getMessageById = async (req, res, next) => {
 
 exports.postNewFriendMessage = async (req, res, next) => {
   const { text } = req.body
-  const { recieverId } = req.params
+  const { friendId } = req.params
   const { user } = req
 
   try {
@@ -216,7 +259,7 @@ exports.postNewFriendMessage = async (req, res, next) => {
       data: {
         creatorId: user.id,
         text,
-        recieverId,
+        recieverId: friendId,
       },
       omit: { groupId: true },
     })
@@ -299,6 +342,117 @@ exports.deleteMessage = async (req, res, next) => {
     })
 
     res.json({ message: 'message deleted' })
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.getMessagesByFriendId = async (req, res, next) => {
+  const { friendId } = req.params
+  const { user } = req
+
+  try {
+    const sentMessages = await prisma.message.findMany({
+      where: {
+        creatorId: user.id,
+        recieverId: friendId,
+      },
+    })
+
+    const recievedMessages = await prisma.message.findMany({
+      where: {
+        creatorId: friendId,
+        recieverId: user.id,
+      },
+    })
+
+    const allMessages = [...sentMessages, ...recievedMessages]
+
+    const sortedMessages = allMessages.sort((a, b) => a.createdAt - b.createdAt)
+
+    res.json(sortedMessages)
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.getMessagesByGroupId = async (req, res, next) => {
+  const { groupId } = req.params
+
+  try {
+    const groupMessages = await prisma.message.findMany({
+      where: {
+        groupId,
+      },
+    })
+
+    const sortedMessages = groupMessages.sort((a, b) => a.createdAt - b.createdAt)
+
+    res.json(sortedMessages)
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.addGroupNewMember = async (req, res, next) => {
+  const { groupId } = req.params
+  const { user } = req
+  const { newMemberId } = req.body
+
+  try {
+    const group = await prisma.group.findFirstOrThrow({
+      where: { id: groupId },
+    })
+
+    if (group.adminId != user.id) throw new ForbiddenError('You are not an admin')
+
+    await prisma.group.update({
+      where: { id: groupId },
+      data: {
+        members: {
+          connect: [{ id: newMemberId }],
+        },
+      },
+    })
+
+    res.json({ message: 'Member added successfully' })
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.removeExistingGroupMember = async (req, res, next) => {
+  const { user } = req
+  const { groupId, memberId } = req.params
+
+  try {
+    const group = await prisma.group.findFirstOrThrow({
+      where: { id: groupId },
+      include: {
+        admin: true,
+        members: true,
+      },
+    })
+
+    if (group.adminId !== user.id) {
+      throw new ForbiddenError('You are not the admin of this group')
+    }
+
+    const memberInGroup = group.members.some((member) => member.id === memberId)
+    if (!memberInGroup) {
+      return res.status(404).json({ message: 'Member not found in the group' })
+    }
+
+    await prisma.group.update({
+      where: { id: groupId },
+      data: {
+        members: {
+          disconnect: { id: memberId },
+        },
+      },
+    })
+
+    res.json({ message: 'Member removed successfully from the group' })
   } catch (error) {
     next(error)
   }
--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -4,14 +4,17 @@ const authConroller = require('../controllers/authController')
 
 authRouter.get('/profile', authConroller.getUserProfile)
 authRouter.get('/friends', authConroller.getUserFriends)
+authRouter.get('/friends/:friendId', authConroller.getFriendById)
 authRouter.get('/groups', authConroller.getUserGroups)
 authRouter.post('/groups', authConroller.createNewGroup)
 authRouter.get('/groups/:groupId', authConroller.getGroupById)
 authRouter.delete('/groups/:groupId', authConroller.deleteGroupById)
 
 authRouter.route('/messages').get(authConroller.getAllMessages)
-authRouter.post('/messages/friend/:friendId', authConroller.postNewFriendMessage)
-authRouter.post('/messages/group/:groupId', authConroller.postNewGroupMessage)
+authRouter.get('/messages/friends/:friendId', authConroller.getMessagesByFriendId)
+authRouter.post('/messages/friends/:friendId', authConroller.postNewFriendMessage)
+authRouter.post('/messages/groups/:groupId', authConroller.postNewGroupMessage)
+authRouter.get('/messages/groups/:groupId', authConroller.getMessagesByGroupId)
 
 authRouter
   .route('/messages/:messageId')
@@ -20,6 +23,8 @@ authRouter
   .delete(authConroller.deleteMessage)
 
 authRouter.patch('/friends/new-friend', authConroller.addNewFriend)
+authRouter.patch('/groups/:groupId/new-member', authConroller.addGroupNewMember)
+authRouter.patch('/groups/:groupId/members/:memberId/delete', authConroller.removeExistingGroupMember)
 authRouter.patch('/friends/:friendId/delete', authConroller.removeExistingFriend)
 
 module.exports = authRouter

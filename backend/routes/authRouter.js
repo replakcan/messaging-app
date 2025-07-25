c645f5160712d1c99243ad8f28ee530edded46c2
replakcan <alper.makcan@gmail.com>
2025-07-25 21:18:58 +0300

    Redesigns messages related routes and separates it's files

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -10,18 +10,6 @@ authRouter.post('/groups', authConroller.createNewGroup)
 authRouter.get('/groups/:groupId', authConroller.getGroupById)
 authRouter.delete('/groups/:groupId', authConroller.deleteGroupById)
 
-authRouter.route('/messages').get(authConroller.getAllMessages)
-authRouter.get('/messages/friends/:friendId', authConroller.getMessagesByFriendId)
-authRouter.post('/messages/friends/:friendId', authConroller.postNewFriendMessage)
-authRouter.post('/messages/groups/:groupId', authConroller.postNewGroupMessage)
-authRouter.get('/messages/groups/:groupId', authConroller.getMessagesByGroupId)
-
-authRouter
-  .route('/messages/:messageId')
-  .get(authConroller.getMessageById)
-  .patch(authConroller.editMessage)
-  .delete(authConroller.deleteMessage)
-
 authRouter.patch('/friends/new-friend', authConroller.addNewFriend)
 authRouter.patch('/groups/:groupId/new-member', authConroller.addGroupNewMember)
 authRouter.patch('/groups/:groupId/members/:memberId/delete', authConroller.removeExistingGroupMember)
---
+++ backend/routes/messagesRouter.js
@@ -0,0 +1,15 @@
+const { Router } = require('express')
+const messagesRouter = Router()
+const messagesController = require('../controllers/messagesController')
+
+messagesRouter.param('messageId', messagesController.attachMessageToRequestObject)
+
+messagesRouter.route('/').get(messagesController.getAllUserMessages).post(messagesController.createNewMessage)
+
+messagesRouter
+  .route('/:messageId')
+  .get(messagesController.getMessageById)
+  .patch(messagesController.editMessageById)
+  .delete(messagesController.deleteMessageById)
+
+module.exports = messagesRouter

0fab1eef49b97dd1855342363387ac90f3290292
replakcan <alper.makcan@gmail.com>
2025-07-25 17:44:25 +0300

    Verifies user on root route
    if there is no logged in user, navigates the user to login-page,
    if there is a user, keeps it in state

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,10 +1,30 @@
-import ChatWindow from '../components/chat-window'
+import { useEffect, useState } from 'react'
 import ContactsContainer from '../components/contacts-container'
 import FiltersContainer from '../components/filters-container'
 import MainContainer from '../components/main-container'
 import SettingsBar from '../components/settings-bar'
+import { axiosInstance } from '../api/axios-instance'
 
 export default function Root() {
+  const [user, setUser] = useState({})
+  const token = JSON.parse(localStorage.getItem('token'))
+  let navigate = useNavigate()
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login')
+    }
+
+    const verifyUser = async () => {
+      await axiosInstance
+        .get('/verify')
+        .then((res) => setUser(res.data))
+        .catch((err) => console.log(err))
+    }
+
+    verifyUser()
+  }, [token, navigate])
+
   return (
     <MainContainer>
       <SettingsBar />

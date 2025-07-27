b8d42c307927cca069e0a5ed341759c47e76e130
replakcan <alper.makcan@gmail.com>
2025-07-27 21:40:05 +0300

    Gives convo-cards active class conditionally on pathname

--- frontend/src/components/conversation-card.jsx
+++ frontend/src/components/conversation-card.jsx
@@ -1,20 +1,29 @@
-import { useNavigate } from 'react-router'
+import { useNavigate, useLocation } from 'react-router'
 import '../styles/conversation-card.css'
 
 export default function ConversationCard({ conversation }) {
-  let navigate = useNavigate()
+  const navigate = useNavigate()
+  const location = useLocation()
+
   const { id, value, type } = conversation
 
+  const isActive =
+    (type === 'group' && location.pathname === `/group/${id}`) ||
+    (type === 'user' && location.pathname === `/chat/${id}`)
+
   const handleClick = () => {
-    if (type == 'group') {
+    if (type === 'group') {
       navigate(`/group/${id}`)
-    } else if (type == 'user') {
+    } else if (type === 'user') {
       navigate(`/chat/${id}`)
     }
   }
 
   return (
-    <div onClick={handleClick} className="conversation-card">
+    <div
+      onClick={handleClick}
+      className={`conversation-card ${isActive ? 'active' : ''}`}
+    >
       <p>{value}</p>
     </div>
   )

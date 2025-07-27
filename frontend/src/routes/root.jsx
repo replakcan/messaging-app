911dbd20aa66b7c11311d54fba5c215e6e959f5a
replakcan <alper.makcan@gmail.com>
2025-07-27 19:59:30 +0300

    Removes verify logic from root.jsx

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,42 +1,18 @@
-import { useEffect, useState } from 'react'
 import ContactsContainer from '../components/contacts-container'
 import FiltersContainer from '../components/filters-container'
 import MainContainer from '../components/main-container'
 import SettingsBar from '../components/settings-bar'
-import { Outlet, useNavigate } from 'react-router'
-import { axiosInstance } from '../api/axios-instance'
-import AuthContext from '../contexts/auth-context'
+import { Outlet } from 'react-router'
 
 export default function Root() {
-  const [user, setUser] = useState({})
-  const token = JSON.parse(localStorage.getItem('token'))
-  let navigate = useNavigate()
-
-  useEffect(() => {
-    if (!token) {
-      navigate('/login')
-    }
-
-    const verifyUser = async () => {
-      await axiosInstance
-        .get('/verify')
-        .then((res) => setUser(res.data))
-        .catch((err) => console.log(err))
-    }
-
-    verifyUser()
-  }, [token, navigate])
-
   return (
-    <AuthContext.Provider value={{ user }}>
-      <MainContainer>
-        <SettingsBar />
-        <FiltersContainer />
-        <ContactsContainer />
-        <div className="outlet-container">
-          <Outlet />
-        </div>
-      </MainContainer>
-    </AuthContext.Provider>
+    <MainContainer>
+      <SettingsBar />
+      <FiltersContainer />
+      <ContactsContainer />
+      <div className="outlet-container">
+        <Outlet />
+      </div>
+    </MainContainer>
   )
 }

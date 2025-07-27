6be1e79765f163c4eb742bbef8435499a03112b9
replakcan <alper.makcan@gmail.com>
2025-07-27 21:39:12 +0300

    Protected-route empties localStorage on unsuccessfull verify

--- frontend/src/components/protected-route.jsx
+++ frontend/src/components/protected-route.jsx
@@ -1,43 +1,39 @@
 import { useEffect, useState } from 'react'
-import { Navigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { axiosInstance } from '../api/axios-instance'
 import AuthContext from '../contexts/auth-context'
 
 export default function ProtectedRoute({ children }) {
   const [authChecked, setAuthChecked] = useState(false)
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [user, setUser] = useState(null)
+  const navigate = useNavigate()
 
   useEffect(() => {
     const verifyToken = async () => {
       const token = JSON.parse(localStorage.getItem('token'))
 
       if (!token) {
-        setAuthChecked(true)
-        setIsAuthenticated(false)
+        navigate('/login', { replace: true })
         return
       }
 
       try {
         const res = await axiosInstance.get('/verify')
         setUser(res.data)
-        setIsAuthenticated(true)
       } catch (error) {
-        setIsAuthenticated(false)
+        localStorage.removeItem('token')
+        navigate('/login', { replace: true })
       } finally {
         setAuthChecked(true)
       }
     }
 
     verifyToken()
-  }, [])
+  }, [navigate])
 
   if (!authChecked) return null
-  if (!isAuthenticated) return <Navigate to="/login" replace />
 
   return (
-    <AuthContext.Provider value={{ user }}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
   )
 }
